376b0fc64f1fa5ebbd4e3779c1d378f4e18092c0
jwillb <jonathanbon@pop-os.localdomain>
2020-07-24 00:13:13 -0600

    Fix time import

--- thinktype.py
+++ thinktype.py
@@ -1,22 +1,22 @@
 import keyboard as kb
-import time
+from time import sleep
 
 def toggle_led(state):
     led = open("/sys/kernel/debug/ec/ec0/io", "wb")
     led.seek(12)
-    
+
     if state:
         led.write(b"\x8a")  # LED On
     else:
         led.write(b"\x0a")  # LED Off
-        
+
     led.flush()
 
 esc_count = 0
 
 def blink_led():
     toggle_led(True)
-    time.sleep(0.1)
+    sleep(0.1)
     toggle_led(False)
 
 keys_list = ["Enter", "Space", "Tab", "Shift", "Alt", "Backspace", "Delete", "esc"]
@@ -38,7 +38,7 @@ while True:
         blink_led()
     elif kb.is_pressed(keys_list[7]):
         esc_count += 1
-    
+
     if esc_count <= 5 and kb.is_pressed(keys_list):
         esc_count = 0
     elif esc_count == 5:
